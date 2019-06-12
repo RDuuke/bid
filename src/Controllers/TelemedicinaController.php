1613c53e4935100094fa37afbb0e09a1d50c1400
rduuke <juuanduuke@gmail.com>
2019-06-12 15:13:18 -0500

    Telemedicina inidcador 1

--- src/Controllers/TelemedicinaController.php
+++ src/Controllers/TelemedicinaController.php
@@ -14,8 +14,7 @@ class TelemedicinaController extends Controller
     {
         $id = Tools::telemedicina_indicador_1();
         dump($id->take(10));
-        dump(array_values(array_values(json_decode(json_encode($id->take(10)->toArray()), true))));
-
+        $a = array_column(json_decode(json_encode($id->take(10)->toArray()), true), "idnotaclinicarespuesta");
         die;
         return $this->view->render($response, "telemedicina/home.twig");
     }
