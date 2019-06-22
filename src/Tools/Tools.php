0123c9d80927d1c6116c08f658bc8a9d9ff7bdcd
RDuuke <juuanduuke@gmail.com>
2019-06-22 16:42:54 -0500

    new opcion inidcador 1 telemedicina"

--- src/Tools/Tools.php
+++ src/Tools/Tools.php
@@ -248,7 +248,7 @@ class Tools
     static public function telemedicina_indicador_1_more()
     {
         $attention = self::sqlTelemedicina("to_char(cupanexo3.fecha, 'YYYY-MM') as ANO_MES, COUNT (*) as total", "ANO_MES", "ANO_MES");
-        $patologias = self::sqlTelemedicina("cie10.codigo as COD, cie10.descripcion as patologia, count(*) as num_atenciones", "COD, patologia", "num_atenciones");
+        $patologias = self::sqlTelemedicina("cie10.codigo as COD, cie10.descripcion as patologia, count(*) as num_atenciones", "COD, patologia", "num_atenciones desc");
         return [
             "atentions" => $attention,
             "patologias" => $patologias
