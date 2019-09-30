7ac2c532dcfd420deaf6cb7477ea4fbe11c1cf68
rduuke <juuanduuke@gmail.com>
2019-09-30 12:21:52 -0500

    fixed bug and update query

--- src/Tools/Tools.php
+++ src/Tools/Tools.php
@@ -238,10 +238,22 @@ class Tools
 
     static public function telemedicina_indicador_1()
     {
-        $patients = self::sqlTelemedicina("COUNT (*) AS total");
+        $patients = Manager::connection("db_telemedicina")
+            ->select(Manager::raw("ca.idcupanexo3, v.fechainicio, ca.fecha, a.numerosolicitud, a.idprioridadatencion, a.enviado as sol_enviada, ca.idestadocupanexo3, ca.idusuario, ca.fechadireccionamiento,
+            ca.idusuario, ca.fechadevolucion, ti.descripcion as tipo_documento, p2.numerodocumento, p2.nombre1, p2.nombre2, p2.apellido1, p2.apellido2, p2.fechanacimiento, p2.sexo, p2.raza, p2.idcoberturasalud,
+            eps.nombre as eps, regexp_replace(nc.subjetivo, E'[\\n\\r]+', ' ', 'g' ) as subjetivo, regexp_replace(nc.objetivo, E'[\\n\\r]+', ' ', 'g' ) as objetivo, 
+            regexp_replace(nc.analisis, E'[\\n\\r]+', ' ', 'g' ) as analisis, regexp_replace(nc.plan, E'[\\n\\r]+', ' ', 'g' ) as plan, cie10.codigo as cie10_codigo, cie10.descripcion as cie10_descripcion,
+            s.nombresede as sede, c.nombre as ciudad, c.codigociudad, ca.idnotaclinicarespuesta, p3.idusuario, ca.idusuarioresidente,  ca.fechaactualizacionusuarioresidente, md.nombre as motivo_devolucion,
+            cup.codigo as cup_codigo, cup.descripcion as cup_descripcion, ca.fechaautorizacion, ca.numeroautorizacion, ca.idusuarioautorizador, ca.idestadoautorizacioncupanexo3"))
+            ->table(Manager::raw("(((((((((((((((visita as v join encuentro as e on v.idvisita = e.idvisita) join anexo3 as a on e.idencuentro = a.idanexo3) join cupanexo3 as ca on a.idanexo3 = ca.idanexo3)
+            join paciente as p on v.idpaciente = p.idpaciente) join persona as p2 on p.idpersona = p2.idpersona)
+            join notaclinica as nc on a.idnotaclinica = nc.idnotaclinica) join tipoidentificacion as ti on ti.idtipoidentificacion = p2.idtipodocumento) join cie10 on nc.idcie10principal = cie10.idcie10)
+            join sedeinstitucion as s on e.idsede = s.idsede) join ciudad as c on s.idmunicipio = c.idciudad) left join departamento as d2 on c.iddepartamento = d2.iddepartamento)
+            join prestador as p3 on e.idprestador = p3.idprestador) left join cup on ca.idcup = cup.idcup) left join eps on eps.ideps = p2.ideps) 
+            left join motivodevolucioncupanexo3 as md on md.idmotivodevolucioncupanexo3 = ca.idmotivodevolucioncupanexo3)"))->get()->count();
         return [
-            "total" => $patients->total,
-            "porcentaje" => round(($patients->total * 100)/self::meta_telemedicina_indicador_1),
+            "total" => $patients,
+            "porcentaje" => round(($patients * 100)/self::meta_telemedicina_indicador_1),
             "meta" => self::meta_telemedicina_indicador_1
         ];
     }
@@ -258,7 +270,6 @@ class Tools
     }
     static public function telemedicina_indicador_2()
     {
-
         $municipios = self::sqlTelemedicina("COUNT (DISTINCT ciudad.nombre) as total");
         return [
             "total" => $municipios->total,
@@ -356,7 +367,7 @@ class Tools
             ->whereIn(
                 "cie10.codigo", TelemeHighCostDisease::all("codigo")->toArray()
             )
-            ->where("notaclinica.fecha", ">=", $fecha)
+            ->where("notaclinica.fechacreacion", ">=", $fecha)
             ->groupBy(Manager::raw('COD, patologia'))
             ->orderByDesc(Manager::raw('num_atenciones'))
             ->get()->toArray();
@@ -365,6 +376,7 @@ class Tools
     }
 
     protected function sqlTelemedicina(String $select, String $groupby = null, String $orderby = null) {
+
         $fecha = "2018-04-30 00:00:00";
         $data = Manager::connection("db_telemedicina")
             ->table("cupanexo3")
